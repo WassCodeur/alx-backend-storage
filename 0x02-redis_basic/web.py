5f40c59cca160e5c1d60cad325df03ac25433bea
WassCodeur <wachioubouraima56@gmail.com>
2023-10-29 15:43:07 +0000

    update advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 """ Implementing an expiring web cache and tracker"""
+from functools import wraps
 import redis
 import requests
+from typing import Callable
+
+_redis = redis.Redis()
 
 
+def count(method: Callable) -> Callable:
+    """decorator to count """
+    @wraps(method)
+    def wrapper(url):
+        count_key = f"count:{url}"
+        cached_key = f"cached:{url}"
+        _redis.incr(count_key)
+        cached = _redis.get(f"{cached_key}")
+
+        if cached:
+            return cached.decode("utf-8")
+        content = method(url)
+        _redis.setex(f"{cached_key}", 10, f"{content}")
+        return content
+    return wrapper
+
+
+@count
 def get_page(url: str) -> str:
     """get page content
 
@@ -13,11 +35,6 @@ def get_page(url: str) -> str:
     Returns:
         str(the page content)
     """
-    key = f"count:{url}"
-    r = redis.Redis()
-    
-    r.incr('Count')
     content = requests.get(url).text
-    r.setex(key, 10, content)
 
     return content
